df562e66aae1a9f51e571eeb97f4fbae793cf923
Vishal <vis.parashar@gmail.com>
2018-06-04 21:24:03 +0530

    added comment for url

--- src/main/java/in/learnspringboot/controller/HelloWorldController.java
+++ src/main/java/in/learnspringboot/controller/HelloWorldController.java
@@ -8,6 +8,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloWorldController {
 	
+	
+//	url to access this contoller is 
+//	http://localhost:8080/home
+	
 	@GetMapping("/home")
 	public String doHome( @RequestParam(value="name",required = false) String name,Model model) {
 		if(name!=null ) {
